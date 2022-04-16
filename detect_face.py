150e42edc925fbcb8433b5c27e5253673467e08f
SmilealniS <tonnam9639@gmail.com>
2022-04-16 13:47:57 +0700

    Update detect_face.py

--- detect_face.py
+++ detect_face.py
@@ -1,5 +1,30 @@
 import cv2
 
+def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, clf):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    features = classifier.detectMultiScale(gray, scaleFactor, minNeighbors)
+    id = 0
+    confidence = 100
+    for (x, y, w, h) in features:
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        id, confidence = clf.predict(gray[y: y + h, x: x + w])
+
+        if confidence > 50:
+            id = 0
+        
+    return img, id, round(100 - confidence)
+
+def conclude(name_results):
+    names_appeared = {name_result[0]: 0 for name_result in name_results}
+    if len(names_appeared) == 1:
+        return list(names_appeared)[0]
+    else:
+        for name_result in name_results:
+            names_appeared[name_result[0]] += 1
+        frequencies = list(names_appeared.values())
+        most_appeared = list(names_appeared.keys())[frequencies.index(max(frequencies))]
+        return most_appeared
+
 faceCascade = cv2.CascadeClassifier("cascade/haarcascade_frontalface_default.xml")
 users = ['Unknown', 'Cheep', 'Champ', 'Peak', 'Tonnam']
 name_results = []
@@ -28,28 +53,3 @@ else:
         print('Authentication failed. Try again.')
         
     cv2.destroyAllWindows()
-
-def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, clf):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    features = classifier.detectMultiScale(gray, scaleFactor, minNeighbors)
-    id = 0
-    confidence = 100
-    for (x, y, w, h) in features:
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        id, confidence = clf.predict(gray[y: y + h, x: x + w])
-
-        if confidence > 50:
-            id = 0
-        
-    return img, id, round(100 - confidence)
-
-def conclude(name_results):
-    names_appeared = {name_result[0]: 0 for name_result in name_results}
-    if len(names_appeared) == 1:
-        return list(names_appeared)[0]
-    else:
-        for name_result in name_results:
-            names_appeared[name_result[0]] += 1
-        frequencies = list(names_appeared.values())
-        most_appeared = list(names_appeared.keys())[frequencies.index(max(frequencies))]
-        return most_appeared
